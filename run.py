0739bfe5b6037bd89bbd659eefd57a42c1625b44
jsm4106 <smmypp11@naver.com>
2025-12-21 22:02:48 +0900

    숫자 클릭 주위 칸 오픈 기능 추가 완료 #2

--- run.py
+++ run.py
@@ -116,32 +116,39 @@ class InputController:
         return -1, -1
 
     def handle_mouse(self, pos, button) -> None:
-        # TODO: Handle mouse button events: left=reveal, right=flag, middle=neighbor highlight  in here
         col, row = self.pos_to_grid(pos[0], pos[1])
         if col == -1:
             return
+
         game = self.game
+        cell = game.board.cells[game.board.index(col, row)]
+
         if button == config.mouse_left:
             game.highlight_targets.clear()
-            game.board.reveal(col,row)
+
+            # ================== Issue #2 추가 ==================
+            # 이미 열린 숫자 칸을 클릭한 경우, 주변 자동 오픈 시도
+            if cell.state.is_revealed and cell.state.adjacent > 0:
+                game.board.reveal_around_if_flag_match(col, row)
+            else:
+                game.board.reveal(col, row)
+
             if not game.started:
                 game.started = True
                 game.start_ticks_ms = pygame.time.get_ticks()
-             
-    
+
         elif button == config.mouse_right:
             game.highlight_targets.clear()
-            game.board.toggle_flag(col,row)
-                
+            game.board.toggle_flag(col, row)
+
         elif button == config.mouse_middle:
-                neighbors = game.board.neighbors(col,row)
-                game.highlight_targets = {
-                    (nc, nr)
-                    for (nc, nr) in neighbors
-                    if not game.board.cells[game.board.index(nc, nr)].state.is_revealed
-                }
-        
-                game.highlight_until_ms = pygame.time.get_ticks() + config.highlight_duration_ms
+            neighbors = game.board.neighbors(col, row)
+            game.highlight_targets = {
+                (nc, nr)
+                for (nc, nr) in neighbors
+                if not game.board.cells[game.board.index(nc, nr)].state.is_revealed
+            }
+            game.highlight_until_ms = pygame.time.get_ticks() + config.highlight_duration_ms
 
 
 class Game:
